3f72f4eb4830367dc2cb0e35b4137d9c56a1f062
Ivan O <ioparin@pm.me>
2022-06-22 22:13:53 +0400

    improving logger to support location propagation

--- Sources/Logger/Logger.swift
+++ Sources/Logger/Logger.swift
@@ -1,7 +1,33 @@
+
 @inlinable
 @inline(__always)
 /// Logs a message to console with specified log type, category and privacy level.
-public func log(_ message: String, logType: OSLogType = .default, category: os.Logger = .default, privacy: _OSLogPrivacy = .private) {
+public func log(
+    _ message: String,
+    logType: OSLogType = .default,
+    category: os.Logger = .default,
+    privacy: _OSLogPrivacy = .private,
+    includeCallerLocation: Bool = true,
+    fileID: String = #fileID,
+    functionName: String = #function,
+    lineNumber: Int = #line
+) {
+    
+    var message = message
+    if includeCallerLocation {
+        let moduleAndFileName = fileID.replacingOccurrences(of: ".swift", with: "")
+        let moduleName = String("\(fileID)".prefix(while: { $0 != "/" }))
+        let fileName = moduleAndFileName
+            .split(separator: "/")
+            .suffix(1)
+            .description
+            .replacingOccurrences(of: "[", with: "")
+            .replacingOccurrences(of: "\"", with: "")
+            .replacingOccurrences(of: "]", with: "")
+        let logLocationDescription = "\(lineNumber):\(moduleName).\(fileName).\(functionName)"
+        message = "\(message) \n> location: \(logLocationDescription)"
+    }
+    
     // privacy argument must be resolved on compile time, hence ugly workaround
     // more info:
     // https://stackoverflow.com/questions/62675874/xcode-12-and-oslog-os-log-wrapping-oslogmessage-causes-compile-error-argumen#63036815
@@ -21,8 +47,25 @@ public func log(_ message: String, logType: OSLogType = .default, category: os.L
 @inlinable
 @inline(__always)
 /// Logs an error to console using predefined error-logger category.
-public func log<E: Error>(_ error: E, privacy: _OSLogPrivacy = .private) where E: CustomStringConvertible {
-    log(error.description, logType: .error, category: .error, privacy: privacy)
+public func log<E: Error>(
+    _ error: E,
+    privacy: _OSLogPrivacy = .private,
+    fileID: String = #fileID,
+    functionName: String = #function,
+    lineNumber: Int = #line
+) where E: CustomStringConvertible {
+    
+    // its important to pass magic ids to log, otherwise location is not forwarded
+    log(
+        error.description,
+        logType: .error,
+        category: .error,
+        privacy: privacy,
+        includeCallerLocation: true,
+        fileID: fileID,
+        functionName: functionName,
+        lineNumber: lineNumber
+    )
 }
 
 
@@ -35,6 +78,40 @@ public func log<Case>(_ case: Case, category: os.Logger = .default, privacy: _OS
 }
 
 
+
+@inlinable
+@inline(__always)
+public func debugEarlyExit(
+    _ message: String,
+    fileID: String = #fileID,
+    functionName: String = #function,
+    lineNumber: Int = #line
+) {
+    #if DEBUG
+    let moduleAndFileName = fileID.replacingOccurrences(of: ".swift", with: "")
+    let moduleName = String("\(fileID)".prefix(while: { $0 != "/" }))
+    let fileName = moduleAndFileName
+        .split(separator: "/")
+        .suffix(1)
+        .description
+        .replacingOccurrences(of: "[", with: "")
+        .replacingOccurrences(of: "\"", with: "")
+        .replacingOccurrences(of: "]", with: "")
+    let guardMessage = "early exit from \(functionName)\n> \(message)"
+    
+    // its important to pass magic ids to log, otherwise location is not forwarded
+    log(
+        guardMessage,
+        logType: .debug,
+        category: .debug,
+        includeCallerLocation: true,
+        fileID: fileID,
+        functionName: functionName,
+        lineNumber: lineNumber
+    )
+    #endif
+}
+
 @inlinable
 @inline(__always)
 /// Logs a debug message to console; Works only in DEBUG build configuration.
@@ -43,7 +120,6 @@ public func debug(_ message: String) {
     log(message, logType: .debug, category: .debug)
     #endif
 }
-
 
 
 
